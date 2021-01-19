81d525875a3b3d87845d42b9651d6f773d9cad61
Olesia <olesia.tk@gmail.com>
2021-01-19 10:58:36 +0200

    Updates

---
+++ precache-manifest.0985d2f730ba87880e63d4104ff85412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d545e6cca06180547745eb5c5337cbd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5dc01271fa2addbb4d5c",
+    "url": "./static/css/main.81d43ce9.chunk.css"
+  },
+  {
+    "revision": "4e3c0e2e69ce97e4426e",
+    "url": "./static/js/2.c63ab0fc.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.c63ab0fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc01271fa2addbb4d5c",
+    "url": "./static/js/main.4d76ccf8.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
